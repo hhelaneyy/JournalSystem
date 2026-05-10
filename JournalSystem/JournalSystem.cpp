4f89f6754752f4203966cc0881abbc7fc4079e6e
Helaney <helaney@yandex.ru>
2026-05-10 13:44:27 +0300

    feat(main): добавлено удобное меню

--- JournalSystem/JournalSystem.cpp
+++ JournalSystem/JournalSystem.cpp
@@ -1,11 +1,40 @@
 ﻿#include <iostream>
 #include "tasks.h"
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "Russian");
-
     init_db();
-    std::cout << "База данных успешно функционирует.";
+    int choice;
+
+    while (true) {
+        system("cls");
+        std::cout << "=============ЭЛЕКТРОННЫЙ ЖУРНАЛ=============" << std::endl;
+        std::cout << "1. Создать группу" << std::endl;
+        std::cout << "0. Выйти из программы" << std::endl;
+        std::cout << "============================================" << std::endl;
+        std::cout << "Ваш выбор: ";
+
+        if (!(std::cin >> choice)) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            continue;
+        }
+
+        switch (choice) {
+        case 1:
+            system("cls");
+            add_group();
+            break;
+        case 0:
+            std::cout << "Выход из программы..." << std::endl;
+            return 0;
+        default:
+            std::cout << "Введите корректное число!" << std::endl;
+        }
+
+        std::cout << "\nНажмите Enter, чтобы продолжить...";
+        std::cin.ignore(1000, '\n');
+        std::cin.get();
+    }
     return 0;
 }
