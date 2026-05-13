d38b838b50f62cf0bf24fb752e95853eed0feab0
Helaney <helaney@yandex.ru>
2026-05-13 13:43:33 +0300

    feat(main): обновил главное меню, а также добавил новый функционал

--- JournalSystem/JournalSystem.cpp
+++ JournalSystem/JournalSystem.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include "tasks.h"
+#include "menus.h"
 
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -9,7 +10,10 @@ int main() {
     while (true) {
         system("cls");
         std::cout << "=============ЭЛЕКТРОННЫЙ ЖУРНАЛ=============" << std::endl;
-        std::cout << "1. Создать группу" << std::endl;
+        std::cout << "1. Управление студентами" << std::endl;
+        std::cout << "2. Управление группами" << std::endl;
+        std::cout << "3. Управление предметами" << std::endl;
+        std::cout << "4. Управление оценками" << std::endl;
         std::cout << "0. Выйти из программы" << std::endl;
         std::cout << "============================================" << std::endl;
         std::cout << "Ваш выбор: ";
@@ -23,9 +27,14 @@ int main() {
         switch (choice) {
         case 1:
             system("cls");
-            add_group();
+            menu2();
+            break;
+        case 2:
+            system("cls");
+            menu1();
             break;
         case 0:
+            system("cls");
             std::cout << "Выход из программы..." << std::endl;
             return 0;
         default:
