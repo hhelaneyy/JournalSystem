aa9af53275f05891f9dadae8b294cc290a0006cc
Helaney <helaney@yandex.ru>
2026-05-13 13:44:31 +0300

    feat(menus & tasks): новые менюшки, плюшки и функции

---
+++ JournalSystem/menus.cpp
@@ -0,0 +1,178 @@
+﻿#include "menus.h"
+#include "tasks.h"
+#include <iostream>
+
+void menu1() {
+    int choice;
+
+    while (true) {
+        system("cls");
+        std::cout << "============УПРАВЛЕНИЕ ГРУППАМИ=============" << std::endl;
+        std::cout << "1. Добавить группу" << std::endl;
+        std::cout << "2. Удалить группу" << std::endl;
+        std::cout << "3. Список групп" << std::endl;
+        std::cout << "0. Выйти из программы" << std::endl;
+        std::cout << "============================================" << std::endl;
+        std::cout << "Ваш выбор: ";
+
+        if (!(std::cin >> choice)) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            continue;
+        }
+
+        switch (choice) {
+        case 1:
+            system("cls");
+            add_group();
+            break;
+        case 2:
+            system("cls");
+            del_group();
+            break;
+        case 3:
+            system("cls");
+            get_groups();
+            break;
+        case 0:
+            system("cls");
+            return;
+        default:
+            std::cout << "Введите корректное число!" << std::endl;
+        }
+
+        std::cout << "\nНажмите Enter, чтобы продолжить...";
+        std::cin.ignore(1000, '\n');
+        std::cin.get();
+    }
+}
+
+void menu2() {
+    int choice;
+
+    while (true) {
+        system("cls");
+        std::cout << "============УПРАВЛЕНИЕ СТУДЕНТАМИ===========" << std::endl;
+        std::cout << "1. Добавить студента" << std::endl;
+        std::cout << "2. Удалить студента" << std::endl;
+        std::cout << "3. Список студентов" << std::endl;
+        std::cout << "0. Выйти из программы" << std::endl;
+        std::cout << "============================================" << std::endl;
+        std::cout << "Ваш выбор: ";
+
+        if (!(std::cin >> choice)) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            continue;
+        }
+
+        switch (choice) {
+        case 1:
+            system("cls");
+            add_student();
+            break;
+        case 2:
+            system("cls");
+            del_student();
+            break;
+        case 3:
+            system("cls");
+            get_students();
+            break;
+        case 0:
+            system("cls");
+            return;
+        default:
+            std::cout << "Введите корректное число!" << std::endl;
+        }
+
+        std::cout << "\nНажмите Enter, чтобы продолжить...";
+        std::cin.ignore(1000, '\n');
+        std::cin.get();
+    }
+}
+
+void menu3() {
+    int choice;
+
+    while (true) {
+        system("cls");
+        std::cout << "===========УПРАВЛЕНИЕ ПРЕДМЕТАМИ============" << std::endl;
+        std::cout << "1. Добавить предмет" << std::endl;
+        std::cout << "2. Удалить предмет" << std::endl;
+        std::cout << "3. Список предметов" << std::endl;
+        std::cout << "0. Выйти из программы" << std::endl;
+        std::cout << "============================================" << std::endl;
+        std::cout << "Ваш выбор: ";
+
+        if (!(std::cin >> choice)) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            continue;
+        }
+
+        switch (choice) {
+        case 1:
+            system("cls");
+            add_group();
+            break;
+        case 2:
+            system("cls");
+            del_group();
+            break;
+        case 3:
+            system("cls");
+            get_groups();
+            break;
+        case 0:
+            system("cls");
+            return;
+        default:
+            std::cout << "Введите корректное число!" << std::endl;
+        }
+
+        std::cout << "\nНажмите Enter, чтобы продолжить...";
+        std::cin.ignore(1000, '\n');
+        std::cin.get();
+    }
+}
+
+void menu4() {
+    int choice;
+
+    while (true) {
+        system("cls");
+        std::cout << "=============УПРАВЛЕНИЕ ОЦЕНКАМИ============" << std::endl;
+        std::cout << "1. Добавить оценки" << std::endl;
+        std::cout << "2. Отредактировать оценки" << std::endl;
+        std::cout << "0. Выйти из программы" << std::endl;
+        std::cout << "============================================" << std::endl;
+        std::cout << "Ваш выбор: ";
+
+        if (!(std::cin >> choice)) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            continue;
+        }
+
+        switch (choice) {
+        case 1:
+            system("cls");
+            add_student();
+            break;
+        case 2:
+            system("cls");
+            del_student();
+            break;
+        case 0:
+            system("cls");
+            return;
+        default:
+            std::cout << "Введите корректное число!" << std::endl;
+        }
+
+        std::cout << "\nНажмите Enter, чтобы продолжить...";
+        std::cin.ignore(1000, '\n');
+        std::cin.get();
+    }
+}
--- JournalSystem/tasks.cpp
+++ JournalSystem/tasks.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <string>
+#include <iomanip>
+#include <format>
 #include <sqlite3.h>
 
 void init_db() {
@@ -67,15 +69,311 @@ void add_group() {
 
 	if (sqlite3_step(stmt) == SQLITE_DONE) {
 		std::cout << "Успех! Группа создана." << std::endl;
+	}
+	else {
+		std::cout << "Возникла неизвестная ошибка: Не удалось создать группу.\nВозможно, группа с таким номером уже создана." << std::endl;
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(DB);
+}
+
+void del_group() {
+	sqlite3* DB;
+	int id;
+
+	if (sqlite3_open("journal.db", &DB) != SQLITE_OK) {
+		std::cerr << "Ошибка подключения к БД: " << sqlite3_errmsg(DB);
+		return;
+	}
+
+	std::cout << "Введите номер группы: ";
+	std::cin >> id;
+	std::cin.ignore();
+
+	sqlite3_stmt* stmt;
+	const char* sql = "DELETE FROM groups WHERE id = ?;";
+
+	sqlite3_prepare_v2(DB, sql, -1, &stmt, 0);
+	sqlite3_bind_int(stmt, 1, id);
+
+	if (sqlite3_step(stmt) == SQLITE_DONE) {
+		if (sqlite3_changes(DB) > 0) {
+			std::cout << "Успех! Группа удалена." << std::endl;
+		}
+		else {
+			std::cout << "Возникла ошибка: Не удалось удалить группу.\nВозможно, группа с таким номером не существует." << std::endl;
+		}
+	}
+	else {
+		std::cout << "Возникла неизвестная ошибка: Не удалось удалить группу." << std::endl;
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(DB);
+}
+
+void get_groups() {
+	sqlite3* DB;
+	if (sqlite3_open("journal.db", &DB) != SQLITE_OK) {
+		std::cerr << "Ошибка открытия БД" << std::endl;
+		return;
+	}
+
+	const char* sql = "SELECT * FROM groups;";
+	sqlite3_stmt* stmt;
+
+	if (sqlite3_prepare_v2(DB, sql, -1, &stmt, 0) != SQLITE_OK) {
+		std::cerr << "Ошибка подготовки запроса: " << sqlite3_errmsg(DB) << std::endl;
+		sqlite3_close(DB);
+		return;
+	}
+
+	std::cout << "\n" << std::left
+		<< std::setw(8) << "ID группы"
+		<< std::setw(20) << "Название группы" << std::endl;
+
+	std::cout << std::string(71, '-') << std::endl;
+
+	while (sqlite3_step(stmt) == SQLITE_ROW) {
+		std::cout << std::left
+			<< std::setw(8) << sqlite3_column_int(stmt, 0)
+			<< std::setw(20) << (const char*)sqlite3_column_text(stmt, 1) << std::endl;
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(DB);
+}
+
+void add_student() {
+	sqlite3* DB;
+	std::string name;
+	int gid, id;
+
+	if (sqlite3_open("journal.db", &DB) != SQLITE_OK) {
+		std::cerr << "Ошибка подключения к БД: " << sqlite3_errmsg(DB);
+		return;
+	}
+
+
+	std::cout << "Введите номер группы: ";
+	std::cin >> gid;
+	std::cin.ignore();
+	std::cout << "Введите ID студента: ";
+	std::cin >> id;
+	std::cin.ignore();
+	std::cout << "Введите студента: ";
+	getline(std::cin, name);
+
+	sqlite3_stmt* stmt;
+	const char* sql = "INSERT INTO students (id, name, group_id) VALUES (?, ?, ?);";
+	sqlite3_stmt* stmt2;
+	const char* sql2 = "SELECT * FROM groups WHERE id=?;";
+
+	sqlite3_prepare_v2(DB, sql, -1, &stmt, 0);
+	sqlite3_prepare_v2(DB, sql2, -1, &stmt2, 0);
+	sqlite3_bind_int(stmt2, 1, gid);
+
+	if (sqlite3_step(stmt2) == SQLITE_ROW) {
+		sqlite3_bind_int(stmt, 1, id);
+		sqlite3_bind_text(stmt, 2, name.c_str(), -1, SQLITE_TRANSIENT);
+		sqlite3_bind_int(stmt, 3, gid);
+
+		if (sqlite3_step(stmt) == SQLITE_DONE) {
+			std::cout << "Успех! Студент добавлен в группу с ID: " << gid << std::endl;
+		}
+		else {
+			std::cout << "Возникла неизвестная ошибка: Не удалось создать студента.\nВозможно, студент с таким номером уже создан." << std::endl;
+		}
+	}
+	else {
+		std::cout << "Возникла неизвестная ошибка: Не удалось создать студента." << std::endl;
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(DB);
+}
+
+void del_student() {
+	sqlite3* DB;
+	int id;
+
+	if (sqlite3_open("journal.db", &DB) != SQLITE_OK) {
+		std::cerr << "Ошибка подключения к БД: " << sqlite3_errmsg(DB);
+		return;
+	}
+
+	std::cout << "Введите ID студента: ";
+	std::cin >> id;
+	std::cin.ignore();
+
+	sqlite3_stmt* stmt;
+	const char* sql = "DELETE FROM students WHERE id = ?;";
+
+	sqlite3_prepare_v2(DB, sql, -1, &stmt, 0);
+	sqlite3_bind_int(stmt, 1, id);
+
+	if (sqlite3_step(stmt) == SQLITE_DONE) {
+		if (sqlite3_changes(DB) > 0) {
+			std::cout << "Успех! Студент удалён." << std::endl;
+		}
+		else {
+			std::cout << "Возникла ошибка: Не удалось удалить студента.\nВозможно, студента с таким ID не существует." << std::endl;
+			std::cout << "\nНажмите Enter, чтобы продолжить...";
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cin.get();
+		}
+	}
+	else {
+		std::cout << "Возникла неизвестная ошибка: Не удалось удалить студента." << std::endl;
 		std::cout << "\nНажмите Enter, чтобы продолжить...";
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		std::cin.get();
 	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(DB);
+}
+
+void get_students() {
+	sqlite3* DB;
+	if (sqlite3_open("journal.db", &DB) != SQLITE_OK) {
+		std::cerr << "Ошибка открытия БД" << std::endl;
+		return;
+	}
+
+	const char* sql = "SELECT * FROM students;";
+	sqlite3_stmt* stmt;
+
+	if (sqlite3_prepare_v2(DB, sql, -1, &stmt, 0) != SQLITE_OK) {
+		std::cerr << "Ошибка подготовки запроса: " << sqlite3_errmsg(DB) << std::endl;
+		sqlite3_close(DB);
+		return;
+	}
+
+	std::cout << "\n" << std::left
+		<< std::setw(8) << "ID студента"
+		<< std::setw(8) << "ФИО"
+		<< std::setw(15) << "ID группы" << std::endl;
+
+	std::cout << std::string(71, '-') << std::endl;
+
+	while (sqlite3_step(stmt) == SQLITE_ROW) {
+		std::cout << std::left
+			<< std::setw(8) << sqlite3_column_int(stmt, 0)
+			<< std::setw(20) << (const char*)sqlite3_column_text(stmt, 1)
+			<< std::setw(8) << sqlite3_column_int(stmt, 2) << std::endl;
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(DB);
+}
+
+void add_subject() {
+	sqlite3* DB;
+	std::string subject;
+	int id;
+
+	if (sqlite3_open("journal.db", &DB) != SQLITE_OK) {
+		std::cerr << "Ошибка подключения к БД: " << sqlite3_errmsg(DB);
+		return;
+	}
+
+
+	std::cout << "Введите ID предмета: ";
+	std::cin >> id;
+	std::cin.ignore();
+	std::cout << "Введите название предмета: ";
+	getline(std::cin, subject);
+
+	sqlite3_stmt* stmt;
+	const char* sql = "INSERT INTO subjects (id, name) VALUES (?, ?);";
+
+	sqlite3_prepare_v2(DB, sql, -1, &stmt, 0);
+
+	sqlite3_bind_int(stmt, 1, id);
+	sqlite3_bind_text(stmt, 2, subject.c_str(), -1, SQLITE_TRANSIENT);
+
+	if (sqlite3_step(stmt) == SQLITE_DONE) {
+		std::cout << "Успех! Предмет добавлен." << std::endl;
+	}
 	else {
-		std::cout << "Возникла неизвестная ошибка: Не удалось создать группу.\nВозможно, группа с таким номером уже создана." << std::endl;
+		std::cout << "Возникла неизвестная ошибка: Не удалось создать студента.\nВозможно, студент с таким номером уже создан." << std::endl;
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(DB);
+}
+
+void del_subject() {
+	sqlite3* DB;
+	int id;
+
+	if (sqlite3_open("journal.db", &DB) != SQLITE_OK) {
+		std::cerr << "Ошибка подключения к БД: " << sqlite3_errmsg(DB);
+		return;
+	}
+
+	std::cout << "Введите ID студента: ";
+	std::cin >> id;
+	std::cin.ignore();
+
+	sqlite3_stmt* stmt;
+	const char* sql = "DELETE FROM students WHERE id = ?;";
+
+	sqlite3_prepare_v2(DB, sql, -1, &stmt, 0);
+	sqlite3_bind_int(stmt, 1, id);
+
+	if (sqlite3_step(stmt) == SQLITE_DONE) {
+		if (sqlite3_changes(DB) > 0) {
+			std::cout << "Успех! Студент удалён." << std::endl;
+		}
+		else {
+			std::cout << "Возникла ошибка: Не удалось удалить студента.\nВозможно, студента с таким ID не существует." << std::endl;
+			std::cout << "\nНажмите Enter, чтобы продолжить...";
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cin.get();
+		}
+	}
+	else {
+		std::cout << "Возникла неизвестная ошибка: Не удалось удалить студента." << std::endl;
 		std::cout << "\nНажмите Enter, чтобы продолжить...";
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		std::cin.get();
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(DB);
+}
+
+void get_subjects() {
+	sqlite3* DB;
+	if (sqlite3_open("journal.db", &DB) != SQLITE_OK) {
+		std::cerr << "Ошибка открытия БД" << std::endl;
+		return;
+	}
+
+	const char* sql = "SELECT * FROM students;";
+	sqlite3_stmt* stmt;
+
+	if (sqlite3_prepare_v2(DB, sql, -1, &stmt, 0) != SQLITE_OK) {
+		std::cerr << "Ошибка подготовки запроса: " << sqlite3_errmsg(DB) << std::endl;
+		sqlite3_close(DB);
+		return;
+	}
+
+	std::cout << "\n" << std::left
+		<< std::setw(8) << "ID студента"
+		<< std::setw(8) << "ФИО"
+		<< std::setw(15) << "ID группы" << std::endl;
+
+	std::cout << std::string(71, '-') << std::endl;
+
+	while (sqlite3_step(stmt) == SQLITE_ROW) {
+		std::cout << std::left
+			<< std::setw(8) << sqlite3_column_int(stmt, 0)
+			<< std::setw(20) << (const char*)sqlite3_column_text(stmt, 1)
+			<< std::setw(8) << sqlite3_column_int(stmt, 2) << std::endl;
 	}
 
 	sqlite3_finalize(stmt);
